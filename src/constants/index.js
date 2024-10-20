cd5937dd8ea9843897236c6cc6ab60806f9a8dae
Raeanne <raeannezou@gmail.com>
2024-10-20 11:26:20 +0800

    Turned images into dynamic links and added to landing page

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,26 +1,28 @@
+import { prof, pubSpeaking, psle, entrepreneurship } from "../utils";
+
 export const programmes = [
   {
     title: "Public Speaking Workshops",
     description: "Impactful speaking skills start here.",
-    image: "https://placehold.co/600x400?text=Public+Speaking+Workshops",
+    image: pubSpeaking,
     alt: "A child holding a microphone and speaking on stage",
   },
   {
     title: "PSLE Power Up Camp",
     description: "An achiever's exam staple.",
-    image: "https://placehold.co/600x400?text=PSLE+Power+Up+Camp",
+    image: psle,
     alt: "An empty classroom with desks and chairs",
   },
   {
     title: "Future Entrepreneurs Labs",
     description: "Defining mindsets during the formative years.",
-    image: "https://placehold.co/600x400?text=Future+Entrepreneurs+Labs",
+    image: entrepreneurship,
     alt: "A young woman holding clothes on hangers",
   },
   {
     title: "Professionals",
     description: "Inspiring your career trajectory.",
-    image: "https://placehold.co/600x400?text=Professionals",
+    image: prof,
     alt: "A group of professionals standing together and smiling",
   },
 ];
--- src/utils/index.js
+++ src/utils/index.js
@@ -5,11 +5,24 @@ import LandingPageBanner from "/images/LandingPageBanner.jpg";
 import mindsphere_Logo from "/images/mindsphere_Logo.png";
 import QuestionnaireHeaderBackground from "/images/QuestionnaireHeaderBackground.png";
 import QuestionnaireBackground from "/images/QuestionnaireBackground.png";
+import psleCamp from "/images/psleCamp.jpg";
+import entrepreneurshipCamp from "/images/entrepreneurshipCamp.jpg";
+import publicSpeaking from "/images/publicSpeaking.jpg";
+import professional from "/images/professional.jpg";
 
 // Export all folders for project use
+// Landing Banner
 export const iea = IEA_Logo;
 export const fundigo = fundigo_Logo;
 export const landingBanner = LandingPageBanner;
 export const mindsphere = mindsphere_Logo;
+
+// Sign Up Process
 export const introBg = QuestionnaireHeaderBackground;
 export const quizBg = QuestionnaireBackground;
+
+// Programmes
+export const psle = psleCamp;
+export const entrepreneurship = entrepreneurshipCamp;
+export const pubSpeaking = publicSpeaking;
+export const prof = professional;
