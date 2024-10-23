f86842558243673f34a8be003f727811f4dd8c32
Raeanne <raeannezou@gmail.com>
2024-10-23 18:29:09 +0800

    Added new background images in and added dynamic links to them

--- src/pages/PersonalisationCoverPage.jsx
+++ src/pages/PersonalisationCoverPage.jsx
@@ -14,7 +14,7 @@ const PersonalisationCoverPage = () => {
         <p className="text-4xl md:text-6xl font-bold text-black mb-6">
           let&#39;s learn more about <span className="text-yellow">you</span>
         </p>
-        <a href="/personalisation/forwho">
+        <a href="/">
           <button className="bg-yellow text-white py-3 px-6 rounded-full text-xl font-semibold">
             Let's Go
           </button>
--- src/utils/index.js
+++ src/utils/index.js
@@ -4,11 +4,15 @@ import fundigo_Logo from "/images/fundigo_Logo.png";
 import LandingPageBanner from "/images/LandingPageBanner.jpg";
 import mindsphere_Logo from "/images/mindsphere_Logo.png";
 import QuestionnaireHeaderBackground from "/images/QuestionnaireHeaderBackground.png";
-import QuestionnaireBackground from "/images/QuestionnaireBackground.png";
 import psleCamp from "/images/psleCamp.jpg";
 import entrepreneurshipCamp from "/images/entrepreneurshipCamp.jpg";
 import publicSpeaking from "/images/publicSpeaking.jpg";
 import professional from "/images/professional.jpg";
+import Child_Q1_Page from "/images/Child_Q1_Page.png";
+import Child_Q2_Page from "/images/Child_Q2_Page.png";
+import Child_Q3_Page from "/images/Child_Q3_Page.png";
+import ChildrenForm from "/images/ChildrenForm.png";
+import Parents_Form from "/images/Parents_Form.png";
 
 // Export all folders for project use
 // Landing Banner
@@ -19,7 +23,11 @@ export const mindsphere = mindsphere_Logo;
 
 // Sign Up Process
 export const introBg = QuestionnaireHeaderBackground;
-export const quizBg = QuestionnaireBackground;
+export const childSurveyBg1 = Child_Q1_Page;
+export const childSurveyBg2 = Child_Q2_Page;
+export const childSurveyBg3 = Child_Q3_Page;
+export const childSurveyHeaderBg = ChildrenForm;
+export const parentSurveyBg = Parents_Form;
 
 // Programmes
 export const psle = psleCamp;
