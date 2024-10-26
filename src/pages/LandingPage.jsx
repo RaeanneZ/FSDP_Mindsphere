01989e4f37229f93018524ec28221701bd9b2c93
Neil Hadziq <99121308+neiqo@users.noreply.github.com>
2024-10-26 21:47:37 +0800

    Typo on import

--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -2,7 +2,7 @@ import Navbar from "../components/Navbar";
 import LandingBanner from "../components/LandingBanner";
 import ProgrammeSection from "../components/ProgrammeSection";
 import Sponsor from "../components/Sponsor";
-import ValueBanner from "../components/valueBanner";
+import ValueBanner from "../components/ValueBanner";
 import Footer from "../components/Footer";
 
 const LandingPage = () => {
