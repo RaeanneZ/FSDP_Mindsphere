0d2d06553b28117f498537e17eb5fad255c1df17
Neil Hadziq <99121308+neiqo@users.noreply.github.com>
2024-10-31 21:03:07 +0800

    Add programme services

--- frontend/src/utils/bookingService.js
+++ frontend/src/utils/bookingService.js
@@ -1,3 +1,22 @@
 import axios from 'axios'
 
 const apiUrl = 'http://localhost:5000/api'
+
+// Programmes methods
+
+const programmeService = {
+    getAllProgrammes: async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/programmes`);
+            return response.data;
+        } catch (err) {
+            console.error('Error getting all programmes: ', err);
+            throw error;
+        }
+    }
+}
+
+
+export {
+    programmeService
+}
