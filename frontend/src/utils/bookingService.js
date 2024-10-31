5900f8ed284e89a0879e120f025a47375ff28b72
Neil Hadziq <99121308+neiqo@users.noreply.github.com>
2024-10-31 21:03:35 +0800

    Add schedule service

--- frontend/src/utils/bookingService.js
+++ frontend/src/utils/bookingService.js
@@ -16,7 +16,30 @@ const programmeService = {
     }
 }
 
+const progScheduleService = {
+    getAllProgSchedules: async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/schedules`);
+            return response.data;
+        } catch (err) {
+            console.error('Error getting all schedules: ', err);
+            throw error;
+        }
+    },
+
+    addProgrammeSchedule: async (newSchedule) => {
+        try {
+          const response = await axios.post(`${apiUrl}/schedules`, newSchedule);
+          return response.data;
+        } catch (err) {
+          console.error('Error adding new schedule: ', err);
+          throw err;
+        }
+      }
+}
+
 
 export {
-    programmeService
+    programmeService,
+    progScheduleService
 }
