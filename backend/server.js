526e2d117cf40232f16406c913d6320bbb8427b7
Neil Hadziq <99121308+neiqo@users.noreply.github.com>
2024-11-01 19:42:17 +0800

    Add paymentemail controller

--- backend/server.js
+++ backend/server.js
@@ -22,6 +22,7 @@ const programmesController = require("./controllers/programmesController");
 const ProgrammeFeedbackController = require("./controllers/programmeFeedBackController");
 const verifyJWT = require("./middlewares/authValidate");
 const paymentEmailRoutes = require("./routes/paymentEmailRoutes");
+const PaymentEmailController = require("./controllers/paymentEmailController");
 const bookingsController = require("./controllers/bookingsController");
 const paymentController = require("./controllers/paymentController");
 const programmesController = require("./controllers/programmesController");
@@ -81,6 +82,8 @@ app.get("/api/feedbacks", ProgrammeFeedbackController.getAllFeedback);
 app.post("/api/postFeedback", ProgrammeFeedbackController.postFeedback);
 
 app.use("/api/payments", paymentEmailRoutes);
+
+PaymentEmailController.sendMembershipCodes;
 
 // Start the server
 app.listen(PORT, async () => {
