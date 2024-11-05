cda96eb2892a30bac8badd706956302170c76f19
Raeanne <raeannezou@gmail.com>
2024-11-05 08:06:36 +0800

    Corrected icons and border radius

--- frontend/src/components/NewsletterPopup.jsx
+++ frontend/src/components/NewsletterPopup.jsx
@@ -1,15 +1,19 @@
 import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faPaperPlane } from "@fortawesome/free-solid-svg-icons";
+import {
+  faClose,
+  faCross,
+  faPaperPlane,
+} from "@fortawesome/free-solid-svg-icons";
 
 const NewsletterPopup = ({ closePopup }) => {
   return (
-    <div className="bg-white p-8 rounded-lg shadow-lg text-center max-w-md mx-auto relative">
+    <div className="bg-lightBlue p-8 rounded-lg shadow-lg text-center max-w-md mx-auto relative">
       <button
         onClick={closePopup}
-        className="absolute top-2 right-2 text-gray-500 text-xl font-bold"
+        className="absolute top-2 right-4 text-gray-500 text-xl font-bold"
       >
-        &times;
+        <FontAwesomeIcon icon={faClose} />
       </button>
       <h1 className="text-2xl font-bold mb-2">
         Subscribe to our mailing list and be updated on upcoming events and
@@ -20,10 +24,10 @@ const NewsletterPopup = ({ closePopup }) => {
         <input
           type="email"
           placeholder="Email"
-          className="w-full p-3 border border-gray-300 rounded-full focus:outline-none focus:ring-2 focus:ring-gray-300"
+          className="w-full p-3 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-gray-300"
         />
         <button className="absolute right-3 top-1/2 transform -translate-y-1/2 text-gray-500">
-          <i className="fas fa-paper-plane"></i>
+          <FontAwesomeIcon icon={faPaperPlane} />
         </button>
       </div>
     </div>
