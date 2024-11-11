78b15067c230cc81e666e2c63ec9cf413b505f21
Neil Hadziq <99121308+neiqo@users.noreply.github.com>
2024-11-11 23:19:16 +0800

    Added survey form call in backendservice

--- frontend/src/utils/backendService.js
+++ frontend/src/utils/backendService.js
@@ -353,6 +353,27 @@ const formService = {
       };
     }
   },
+
+  addSurvey: async (email, howHear, expRating, feedbackText) => {
+    try {
+      const newSurvey = {
+        email: email,
+        howHear: howHear,
+        expRating: expRating,
+        feedbackText: feedbackText,
+      };
+
+      const response = await axios.post(`${apiUrl}/survey`, newSurvey);
+      return response.data;
+    } catch (err) {
+      console.error("BackendService: Error adding new survey: ", err);
+      return {
+        success: false,
+        message: "Adding survey failed",
+        error: err.response ? err.response.data : err.message,
+      };
+    }
+  },
 };
 
 export default {
@@ -363,4 +384,5 @@ export default {
   bookingService,
   paymentService,
   newsletterService,
+  formService
 };
