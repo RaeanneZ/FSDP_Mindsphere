907cd7a248c91637e1f549e6c2129a6dacf00afe
Hendrik Yong <hendrikyong1205@gmail.com>
2024-11-13 12:05:26 +0800

    Add endpoint to retrieve registered programmes by account and update landing page

--- backend/controllers/programmesController.js
+++ backend/controllers/programmesController.js
@@ -1,15 +1,28 @@
-const Programmes = require("../models/programmes")
+const Programmes = require("../models/programmes");
 
 const getAllProgrammes = async (req, res) => {
-    try {
-        const programmes = await Programmes.getAllProgrammes();
-        res.status(200).json(programmes);
-    } catch (err) {
-        console.error(err);
-        res.status(500).send("ControllerError: Error retreiving programmes");
-    }
-}
+  try {
+    const programmes = await Programmes.getAllProgrammes();
+    res.status(200).json(programmes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("ControllerError: Error retreiving programmes");
+  }
+};
+
+const getRegisteredProgrammesByAccount = async (req, res) => {
+  try {
+    const programmes = await Programmes.getRegisteredProgrammesByAccount(
+      req.params.email
+    );
+    res.status(200).json(programmes);
+  } catch (err) {
+    console.error(err);
+    res.status(500).send("ControllerError: Error retreiving programmes");
+  }
+};
 
 module.exports = {
-    getAllProgrammes
-}
+  getAllProgrammes,
+  getRegisteredProgrammesByAccount,
+};
