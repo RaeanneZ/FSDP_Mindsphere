1f57d108faec3833db9ea067ca455610533bcf9a
Raeanne <raeannezou@gmail.com>
2024-11-14 14:31:36 +0800

    Uncommented neil's tailscale network

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -11,7 +11,9 @@ export default defineConfig({
     port: 5173,
     proxy: {
       "/api": {
-        //target: isLocalhost ? 'http://localhost:5000' : 'http://100.97.230.39:5000', // neil tailscale network laptop: http://100.83.156.26:5000
+        target: isLocalhost
+          ? "http://localhost:5000"
+          : "http://100.97.230.39:5000", // neil tailscale network laptop: http://100.83.156.26:5000
         changeOrigin: true,
       },
     },
