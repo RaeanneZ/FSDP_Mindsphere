a721f1806098da2446052e1bb04570c86025a77a
Raeanne <raeannezou@gmail.com>
2025-01-02 21:46:37 +0800

    Added key into page (untested)

--- frontend/src/pages/AccountManagementPage.jsx
+++ frontend/src/pages/AccountManagementPage.jsx
@@ -19,6 +19,21 @@ const AccountManagementPage = () => {
   const [children, setChildren] = React.useState([1]);
   const [autocomplete, setAutocomplete] = React.useState(null); // State for Autocomplete
 
+  // Load key on render page
+  useEffect(() => {
+    const loadGoogleMapsScript = () => {
+      const script = document.createElement("script");
+      script.src = `https://maps.googleapis.com/maps/api/js?key=${
+        import.meta.env.VITE_GOOGLE_CLOUD_API_KEY
+      }&libraries=places`;
+      script.async = true;
+      script.defer = true;
+      document.body.appendChild(script);
+    };
+
+    loadGoogleMapsScript();
+  }, []);
+
   // This is for parent form
   const [formData, setFormData] = React.useState({
     name: "",
