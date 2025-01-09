a60f674b371f29b7b471b2e921a54f89c93620b4
Neil Hadziq <99121308+neiqo@users.noreply.github.com>
2025-01-10 01:54:41 +0800

    Added service for dashboard metrics

--- frontend/src/utils/backendService.js
+++ frontend/src/utils/backendService.js
@@ -457,6 +457,19 @@ const programmeFeedBackService = {
     },
 };
 
+const dashboardService = {
+    getDashboardMetrics: async () => {
+        try {
+            const response = await axios.get(`${apiUrl}/dashboard-metrics`);
+            return response.data;
+        } catch (err) {
+            console.error("BackendService: Error retrieving dashboard metrics: "),
+            err
+        };
+        throw err;
+    }
+}
+
 export default {
     programmeService,
     progScheduleService,
