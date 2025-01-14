4dff7c87c4bacab9cb8d4c0eb5e9b103e27115ff
Raeanne <raeannezou@gmail.com>
2025-01-14 10:49:55 +0800

    Reformatted constant data and made necessary changes to the B2B in preparation of Backend data

--- frontend/src/pages/B2BEnquiryTimelineTracker.jsx
+++ frontend/src/pages/B2BEnquiryTimelineTracker.jsx
@@ -5,42 +5,28 @@ const timelineData = [
   {
     text: "Sent Enquiry",
     date: "January 01 2025",
-    category: {
-      tag: "Completed",
-      color: "#018f69",
-    },
+    tag: "Completed",
     link: {
       url: "https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2",
-      text: "View Document Here",
     },
   },
   {
     text: "Proposal Draft 1",
     date: "January 18 2025",
-    category: {
-      tag: "Pending",
-      color: "#edc55b",
-    },
+    tag: "Pending",
     link: {
       url: "https://medium.com/@popflorin1705/javascript-coding-challenge-1-6d9c712963d2",
-      text: "View Document Here",
     },
   },
   {
     text: "Finalize Proposal",
     date: "Feburary 03 2025",
-    category: {
-      tag: "Incomplete",
-      color: "#dbe0e2",
-    },
+    tag: "Incomplete",
   },
   {
     text: "Full Payment",
     date: "March 03 2025",
-    category: {
-      tag: "Incomplete",
-      color: "#dbe0e2",
-    },
+    tag: "Incomplete",
   },
 ];
 
