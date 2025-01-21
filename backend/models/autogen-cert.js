7680f136ecd6901147bce829d7b5a2bbc52b192e
Hendrik Yong <hendrikyong1205@gmail.com>
2025-01-21 20:36:49 +0800

    Add autogenCert model for certificate generation

---
+++ backend/models/autogen-cert.js
@@ -0,0 +1,20 @@
+const sql = require("mssql");
+const dbConfig = require("../dbConfig");
+
+const PDFDocument = require("pdfkit");
+const fs = require("fs");
+const path = require("path");
+
+class autogenCert {
+  constructor(Name) {
+    this.Name = Name;
+  }
+
+  static async generateCert() {
+    try {
+        
+    } catch {
+
+    }
+  }
+}
