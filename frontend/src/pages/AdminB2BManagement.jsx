b85db17ade0175f23fa8a17f9edc65779fd46d25
Raeanne <raeannezou@gmail.com>
2025-01-30 22:32:54 +0800

    Updated UI for the admin b2b management

--- frontend/src/pages/AdminB2BManagement.jsx
+++ frontend/src/pages/AdminB2BManagement.jsx
@@ -83,7 +83,7 @@ const AdminB2BManagement = () => {
         className="bg-cover flex justify-center items-center"
         style={{ backgroundImage: `url(${introBg})` }}
       >
-        <div className="max-w-2xl mx-auto p-6 space-y-6">
+        <div className="max-w-2xl w-full p-6 space-y-6">
           <h1 className="text-2xl font-bold">B2B Enquiries</h1>
           {/* This is to target Business */}
           <div>
